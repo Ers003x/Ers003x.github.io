504c63a26edd1747c6dcfdeabba146ecd10c54a3
Ersjan Kerri <63121053+Ers003x@users.noreply.github.com>
2020-09-17 16:37:54 +0200

    Update settings.py

--- Invaders/settings.py
+++ Invaders/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^+zkx!oe+@=j!@q@ts8#f^hlobowe=&2&ew+=kp^5iquslxg@*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
